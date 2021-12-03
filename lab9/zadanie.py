52f9b06529b9c12ff60312a57c365094ecebbbe7
KarolinaPondel <k.m.pondel@gmail.com>
2021-12-03 11:45:44 +0100

    komentarze do zadań lab9

--- lab9/zadanie.py
+++ lab9/zadanie.py
@@ -8,7 +8,7 @@ class Ksiazka_telefoniczna:
         self.slownik = {}
 
     def add_record(self, imie, numer):
-        if numer.isdigit():
+        if numer.isdigit(): # 'int' object has no attribute 'isdigit', powinno być: str(numer)
             if imie in self.slownik.values():
                 print("osoba jest w ksiazce telefonicznej")
             else:
@@ -21,12 +21,13 @@ class Ksiazka_telefoniczna:
         for i in self.slownik:
             print("dane", i, ":", "numer", self.slownik[i])
 
-    def print_json(self, son):
-        with open(son, "r+") as file:
+    def print_json(self, son): # write_json, tak miała nazywać się ta funkcja
+        # self.son = son --> tego brakuje
+        with open(son, "r+") as file: # jeżeli plik nie istnieje otrzymujemy błąd 'No such file or directory' co oznacza, że ta funkcja nie tworzy nowego pliku, tylko szuka go w obecnym katalogu, a powinna tworzyć plik i zapisywać do niego wynik, powinno być open(son, "w")
             json.dump(self.slownik, file)
 
     def read_json(self, son):
-        with open(son, "r+") as file:
+        with open(son, "r+") as file: # FileNotFoundError: No such file or directory; zła kolejność: funckja powinna najpierw sprawdzać, czy plik istnieje, a potem próbować go otworzyć lub wyświetlać komunikat
             if os.path.isfile(son):
                 slownik = json.load(file)
                 for j in slownik:
@@ -53,3 +54,55 @@ if __name__ == "__main__":
     soon = input("podaj nazwe pliku: ")
     obiekt.read_json(soon)
     obiekt.print_all()
+
+# funkja write_json() mogłaby wyglądać tak:
+'''
+def write_json(self, son):
+    self.son = son
+    with open(self.son, "w") as file:
+        json.dump(self.slownik, file)
+'''
+# a jej wywołanie:
+'''
+obiekt.write_json('zadanie.json') 
+'''
+# funckja read_json() mogłaby wyglądać tak:
+'''
+def read_json(self, son):
+    if os.path.isfile(son):
+        with open(son, "r+") as file:
+            slownik = json.load(file)
+            for j in slownik:
+                self.add_record(j, slownik[j])
+    else:
+        print("plik nie istnieje")
+'''
+# wywołanie funckji read_json():
+'''
+obiekt.read_json('nowy.json') # plik 'nowy.json' to plik swotrzony ręcznie przechowujący nowy słównik, który zostaje dodany do istniejącego słownika
+'''
+# brak podpunktu f); metody '__add__(self, other); mogłaby np. wyglądać tak:
+'''
+ def __add__(self, other):
+        new = Ksiazka_telefoniczna()
+        for record in self.slownik:
+            new.add_record(record, self.slownik[record])
+        for record in other.slownik:
+            new.add_record(record, other.slownik[record])
+        return new
+'''
+# testowanie 'if __name__ == "__main__":' jest ok, ale wygodniej byłoby gdyby nie było input() tylko podanie argumentów przy wywołaniu funcji:
+'''
+if __name__ == "__main__:
+    obiekt = Ksiazka_telefoniczna()
+
+    obiekt.add_record("Ania", 1111)
+    obiekt.add_record("Ola", 2222)
+    obiekt.add_record("Ania", 3333) # test czy dodaje rekord, jeśli dane imię jest już w słowniku
+    obiekt.add_record("Ela", '4444cc') # test czy liczba.isdigit() działa
+    obiekt.print_all()
+
+    obiekt.write_json('ksiazka1.json') # ksiazka1.json -> plik, do którego zostaje zapisany słównik
+    obiekt.read_json('nowy.json') # plik 'nowy.json' to plik swotrzony ręcznie przechowujący nowy słównik, który zostaje dodany do istniejącego słownika (test)
+    obiekt.print_all()
+'''
