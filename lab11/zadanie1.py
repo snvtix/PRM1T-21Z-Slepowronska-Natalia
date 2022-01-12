40b05c8e5213440155dadb60df34f6e11ad1b1cd
KarolinaPondel <karolina.pondel@wp.pl>
2022-01-12 09:51:15 +0000

    komentarze do zadań lab11

--- lab11/zadanie1.py
+++ lab11/zadanie1.py
@@ -1,15 +1,16 @@
-import os.path
+import os.path # import os, bo z tej biblioteki są wykorzystywane rózne funkcje, nie tylko path
+
 def file_sizes(path):
     d = {}
     if os.path.isdir(path):
         l = os.listdir(path)
         for i in l:
             if os.path.isfile(f"{path}/{i}"):
-                enl = os.path.splitext(f"{path}/{i}")
-                print(enl)
-                enll = enl[1]
+                enl = os.path.splitext(f"{path}/{i}") # tu powinno być: enl = os.path.splitext(i)
+                print(enl) # niepotrzebne
+                enll = enl[1] # moze byc, ale tez niepotrzebne
                 size = os.path.getsize(f"{path}/{i}")
-                d.get(enl)
+                d.get(enl) # niepotrzebne
                 if enl in d.keys():
                     d[enll] = v
                     v = v + size
@@ -21,5 +22,42 @@ def file_sizes(path):
         assert os.path.isdir(path), "nie jest to katalog"
 
 if __name__=='__main__':
-    a = str(input())
+    a = str(input()) # tutaj nie miał być input() tylko podanie jako argumentu do funkcji ściezki do katalogu: 'books/poe'
     print(file_sizes(a))
+
+# brak try except w kodzie testującym
+# uzyskano zły wynik:
+'''
+('books/poe/beczka-amontillada', '.txt')
+('books/poe/czarny-kot', '.mobi')
+('books/poe/studnia-i-wahadlo', '.pdf')
+('books/poe/zaglada-domu-usherow', '.txt')
+'''
+# mozliwe poprawne rozwiazanie:
+'''
+import os
+
+def file_sizes(path):
+    d = {}
+    if os.path.isdir(path):
+        l = os.listdir(path)
+        for i in l:
+            if os.path.isfile(f"{path}/{i}"):
+                enl = os.path.splitext(i)
+                size = os.path.getsize(f"{path}/{i}")
+                if enl[1] in d:
+                    d[enl[1]] += size
+                else:
+                    d[enl[1]] = size
+        return d
+    else:
+        assert os.path.isdir(path)
+
+
+if __name__ == "__main__":
+    try:
+        print(file_sizes('books/poe')) # test działania programu
+        print(file_sizes('book')) # test obsługi wyjątku
+    except AssertionError:
+        print('nie jest to katalog')
+'''
--- lab11/zadanie3.py
+++ lab11/zadanie3.py
@@ -10,15 +10,61 @@ def rn(x,y):
         assert "maja rozna dlugosc"
 
     for j in yprim:
-        suma = suma + yprim[j]*yprim[j]
-
+        suma = suma + yprim[j]*yprim[j] # "TypeError: list indices must be integers or slices, not float", poza tym to mialo byc w kodzie testujacym
+        # j to kolejne wartosci z listy yprim, nie indeksy, jeśli chcemy mieć dostęp do indeksów, nalezy wykorzystać enumerate():
+        '''
+        for idx, val in enumerate(yprim):
+            uma = suma + yprim[idx]*yprim[idx]
+        '''
+        # ale wystarczy zrobić:
+        '''
+        for j in yprim:
+            suma = suma + j*j
+        '''
     print(suma)
-    wynik = math.sqrt(suma)
+    wynik = math.sqrt(suma) # to mialo byc w kodzie testujacym
     print(wynik)
+
+    # funkcja nic nie zwraca (nie ma return), a powinna zwracac listę yprim
 
 if __name__=="__main__":
 
     x = [1,2,3,4]
-    y = [0.5,1,2,3.5]
+    y = [0.5,1,2,3.5] 
 
     print(rn(x,y))
+
+# poprawne rozwiązanie:
+'''
+import math
+
+def rn(x,y):
+
+    yprim = []
+
+    assert len(x) == len(y)
+
+    for i in range(0, len(x)-1):
+        yprim.append((y[i+1]-y[i])/(x[i+1]-x[i]))
+    
+    return yprim
+
+
+if __name__=="__main__":
+
+    x = [1,2,3,4]
+    y = [0.5,1,2,3.5] 
+
+    try:
+        yprim = rn(x,y)
+
+        suma = 0
+        for j in yprim:
+            suma = suma + j*j 
+
+        wynik = math.sqrt(suma)
+        print(wynik)
+        
+    except AssertionError:
+        print('maja rozna dlugosc')
+'''
